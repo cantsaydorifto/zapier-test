14cef6c1a871d88a36f265ee7be22f342ee4cd5c
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2025-01-09 12:32:24 +0530

    del

--- api/index.js
+++ api/index.js
@@ -1,19 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import supabase from "../utils/supabase";
 
 const app = new Hono().basePath("/api");
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
-});
-
-app.post("/add", async (c) => {
-  const { data, error } = await supabase.from("todo").insert({});
-  if (error) {
-    throw { status: 500, message: "Error creating todo" };
-  }
-  return c.json(data);
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
 });
 
 const handler = handle(app);
--- utils/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  Bun.env.SUPABASE_URL as string,
-  Bun.env.SUPABASE_KEY as string
-);
-
-export default supabase;
