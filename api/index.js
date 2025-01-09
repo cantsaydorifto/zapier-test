5f5fe2ba2521c9cc4885d34f28bcf088e3c73a56
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2025-01-09 12:51:12 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import { createClient } from "@supabase/supabase-js";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
@@ -6,8 +7,19 @@ const app = new Hono().basePath("/api");
 app.get("/", (c) => {
   return c.json({ message: "Hello World!" });
 });
-app.post("/apples", (c) => {
-  return c.json({ message: "Hello Apples!" });
+
+app.post("/add", async (c) => {
+  try {
+    const { SUPABASE_URL, SUPABASE_KEY } = env < { NAME: string } > c;
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+    const { data, error } = await supabase.from("todo").insert({});
+    if (error) {
+      throw { status: 500, message: "Error creating todo" };
+    }
+    return c.json({ message: "Hello Apples!" });
+  } catch (error) {
+    throw { status: 500, message: "Error creating todo" };
+  }
 });
 
 const handler = handle(app);
